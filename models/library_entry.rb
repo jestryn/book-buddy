343bb507204b5c640d980393b19c2b5f2cbf9dd0
Dustin Kerbel <jestryn@msn.com>
2025-10-03 08:25:33 -0700

    update LibraryEntry class to hold data from google api

--- models/library_entry.rb
+++ models/library_entry.rb
@@ -1,23 +1,30 @@
+# models/library_entry.rb
 require 'securerandom'
 
 class LibraryEntry
-  attr_accessor :id, :title, :author, :status, :notes
+  attr_accessor :id, :google_id, :title, :authors, :thumbnail, :status, :notes, :added_date
 
-  def initialize(id: nil, title:, author:, status: 'unread', notes: "")
-    @id = id || SecureRandom.uuid
+  def initialize(id: nil, google_id: nil, title:, authors:, thumbnail: nil, status: 'unread', notes: "", added_date: nil)
+    @id = id
+    @google_id = google_id
     @title = title
-    @author = author
+    @authors = authors
+    @thumbnail = thumbnail
     @status = status
     @notes = notes
+    @added_date = added_date
   end
 
   def to_h
     {
       id: id,
+      google_id: google_id,
       title: title,
-      author: author,
+      authors: authors,
+      thumbnail: thumbnail,
       status: status,
-      notes: notes
+      notes: notes,
+      added_date: added_date
     }
   end
 end
